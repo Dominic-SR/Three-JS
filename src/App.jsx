4a68325cfdc07396d5d6b55654b3ee239f5244d4
dominic <dominicseduraja3@gmail.com>
2024-09-24 09:03:26 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Project from './Pages/Project'
+
+
+const App = () => {
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/project' element={<Project />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  )
+}
+
+export default App
